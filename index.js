115c9c8e30e709948313a1150581ff29930dcb12
Muhammad Mudassar Hameed <57958613+Mudassar1999@users.noreply.github.com>
2023-11-08 11:45:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,29 @@
-// const express = require("express");
-// const { createServer } = require("http");
-
-// const app = express();
-
-// const http = createServer(app);
-
-// const PORT = process.env.PORT || 3000;
-// http.listen(PORT, () => {
-//   console.log(`Listening on Port ${PORT}`);
-// });
-
-// app.use(express.static(__dirname + "/public"));
-
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/index.html");
-// });
-
-// // Server Socket/IO
-
-// const io = require("socket.io")(http);
-
-// io.on("connection", (socket) => {
-//   console.log("New User Connected");
-//   socket.on('message',(msg)=>{
-//     console.log(msg);
-//     socket.broadcast.emit('message',msg);
-//   })
-// });
 const express = require("express");
-var http = require("http");
+const { createServer } = require("http");
+
 const app = express();
-const port = process.env.PORT || 5000;
-var server = http.createServer(app);
-var io = require("socket.io")(server);
 
-//middlewre
-app.use(express.json());
-var clients = {};
+const http = createServer(app);
 
-io.on("connection", (socket) => {
-  console.log("connetetd");
-  console.log(socket.id, "has joined");
-  // socket.on("signin", (id) => {
-  //   console.log(id);
-  //   clients[id] = socket;
-  //   console.log(clients);
-  // });
-  socket.on("message", (msg) => {
-    console.log(msg);
-    let targetId = msg.targetId;
-    if (clients[targetId]) clients[targetId].emit("message", msg);
-  });
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
 });
 
-server.listen(port, "0.0.0.0", () => {
-  console.log("server started");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Server Socket/IO
+
+const io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+  console.log("New User Connected");
+  socket.on('message',(msg)=>{
+    console.log(msg);
+    socket.broadcast.emit('message',msg);
+  })
 });
